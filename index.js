40d33fee2de81e680fb02e6eddfc6685c17860de
Julie M <seejulielee@gmail.com>
2022-12-01 16:42:13 -0800

    Refactored rendered order method

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import { menuArray } from './data.js'
 
 //shows the order section of the app and gets control of the id of the clicked add button, passing the id to the handleAddClick function
 document.addEventListener('click',function(event){
-  const orderDiv = document.getElementById("order-div")
-  orderDiv.classList.remove("hide-order")
   if(event.target.dataset.add) {
     handleAddClick(event.target.dataset.add)
   }
+  renderOrder()
 })
 
+//holds the clicked items (aka the order) - works.
 const order =[]
 //filters the menuArray data to take control of the correct menu object, then pushes the target object into the order array
 function handleAddClick(itemId){
@@ -17,23 +17,25 @@ function handleAddClick(itemId){
         return menuItem.id == itemId    
   })
   order.push(targetMenuObj)
-  //console.log(order)
+}
+//console.log(Object.values(order))
+//Shows the order section, and maps over the order while rendering HTML based on what's in the array
+function getOrderHtml() {
+  const orderDiv = document.getElementById("order-div")
+  orderDiv.classList.remove("hide-order")
+  //let orderItems = ''
+  
+  //Object.values(order).forEach(function(orderItem) {
+    //console.log(orderItem)
+   // orderItems += `
+    //<p>${orderItem.name}</p>`
+  
+  //return orderItems  
 }
 
-
-
-
-
-
-   
-  
-  
-    
- 
-  
-
-
-
+function renderOrder() {
+  document.getElementById('order').innerHtmL = getOrderHtml()
+}
 
 function getHtml() {
   let html = ''
@@ -58,13 +60,4 @@ function renderMenu() {
 }
 renderMenu()
 
-function renderOrder() {
-  document.getElementById('order').innerHtmL = `
-    <p>${order.name}</p>
-    <p>$${order.price}</p>
-    `
-
-}
-
-renderOrder()
   
