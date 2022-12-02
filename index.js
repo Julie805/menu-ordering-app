ff1d5c079102ebb5978829685ceb3462b996453c
Julie M <seejulielee@gmail.com>
2022-12-02 10:29:08 -0800

    Styled order rendering

--- index.js
+++ index.js
@@ -1,46 +1,21 @@
 
 import { menuArray } from './data.js'
-
-//shows the order section of the app and gets control of the id of the clicked add button, passing the id to the handleAddClick function
-document.addEventListener('click',function(event){
-  if(event.target.dataset.add) {
-    handleAddClick(event.target.dataset.add)
-  }
-  renderOrder()
-})
+//https://scrimba.com/scrim/cpJBkpUd
 
 //holds the clicked items (aka the order) - works.
-const order =[]
-//filters the menuArray data to take control of the correct menu object, then pushes the target object into the order array
-function handleAddClick(itemId){
-  const targetMenuObj = menuArray.filter(function(menuItem){
-        return menuItem.id == itemId    
-  })
-  order.push(targetMenuObj)
-}
-//console.log(Object.values(order))
-//Shows the order section, and maps over the order while rendering HTML based on what's in the array
-function getOrderHtml() {
-  const orderDiv = document.getElementById("order-div")
-  orderDiv.classList.remove("hide-order")
-  //let orderItems = ''
-  
-  //Object.values(order).forEach(function(orderItem) {
-    //console.log(orderItem)
-   // orderItems += `
-    //<p>${orderItem.name}</p>`
-  
-  //return orderItems  
-}
+const orderArray =[]
 
-function renderOrder() {
-  document.getElementById('order').innerHtmL = getOrderHtml()
-}
+//shows the order section of the app and gets control of the id of the clicked add button, passing the id to the addMenuItem function
+document.addEventListener('click',function(event){
+  if(event.target.dataset.add) {
+    addMenuItem(event.target.dataset.add)
+  }
+})
 
-function getHtml() {
-  let html = ''
+function getMenuHtml() {
+  let menuHtml = ''
   menuArray.forEach(function(menuItem) {
-      html +=
+      menuHtml +=
       `<div class="menu-item">
           <p class="emoji">${menuItem.emoji}</p>
           <div>
@@ -52,11 +27,51 @@ function getHtml() {
       </div>
       <hr></hr>`
   })
-  return html
+  return menuHtml
+}
+
+//Maps over the order array while rendering HTML based on what's in the array
+function getOrderHtml() {
+  let totalPrice = 0
+  let orderHtml = `<h3 class="center">Your order</h3>`
+  //adds items from the orderArray
+  orderArray.forEach(function(item, index) {
+    orderHtml += `
+      <div class="order">
+        <h3>${item.name}<span class="remove-btn" data-index=${index}>remove</span></h3
+        <h3 class="price">$${item.price}</h3>
+      </div>`
+      totalPrice += item.price
+  })
+  orderHtml += `
+    <div>
+      <hr>
+      <div class="order-line">
+      <h3>Total price:</h3>
+      <h3>$${totalPrice}</h3>
+      </div>
+    </div>
+    <button>Complete order</button>
+    `  
+  return orderHtml  
+   
+}
+
+
+//filters the menuArray data to take control of the correct menu object, then pushes the target object into the order array
+function addMenuItem(itemId){
+  orderArray.push(menuArray.filter(function(menuItem){
+    return menuItem.id == itemId 
+  })[0])
+  renderOrder()
+}
+
+function renderOrder() {
+  document.getElementById('order-container').innerHTML = getOrderHtml()
 }
 
 function renderMenu() {
-  document.getElementById('menu').innerHTML = getHtml()
+  document.getElementById('menu').innerHTML = getMenuHtml()
 }
 renderMenu()
 
